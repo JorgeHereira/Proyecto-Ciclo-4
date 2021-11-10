716eb9cc018f4d1df3d8ca14340c3a00fc470e84
JorgeHereira <j.hereira@hotmail.com>
2021-11-09 23:46:38 -0500

    cuarto commit

---
+++ empleados_front/src/components/navbar/navbar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  Container,
+  Dropdown,
+  DropdownButton,
+  Nav,
+  Navbar,
+  Row,
+  Col,
+} from "react-bootstrap";
+import "./navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+
+import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
+
+export default class TopMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <Navbar fixed="top" id="navbar" bg="primary" variant="dark">
+        <Container>
+          <img className="logo" src="../img/DiseñodeHormigónarmado2-Logo_UIS-401685.png" />
+          <Navbar.Brand href="#home">
+            Grupo 14 MisionTic<spam id="usuario-sub-branm"></spam>
+          </Navbar.Brand>
+          <NavbarToggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              {/*<Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>*/}
+            </Nav>
+
+            <DropdownButton id="dropdown-basic-button" title="Usuario">
+              <Dropdown.Header id="dropdown-header">
+                <Row>
+                  <FontAwesomeIcon icon={faUserCircle} />
+                </Row>
+                <Row>Usuario</Row>
+              </Dropdown.Header>
+              <Dropdown.Divider />
+              <Dropdown.Item href="#/action-1">Cerrar sesión</Dropdown.Item>
+              {/*<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>*/}
+            </DropdownButton>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
