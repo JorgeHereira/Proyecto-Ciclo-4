c967a53c5a6a2b1bc23e880e7e22a753672595ad
JorgeHereira <j.hereira@hotmail.com>
2021-11-10 07:39:33 -0500

    Jorge 10/11 commit

--- empleados_front/src/components/router/router.js
+++ empleados_front/src/components/router/router.js
@@ -1,4 +1,13 @@
-import React from 'react';
-import {BrowserRouter as Router
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "../login/login";
 
+export default function AppRouter() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path={["/", "/login"]} component={Login} />
+      </Switch>
+    </Router>
+  );
 }
